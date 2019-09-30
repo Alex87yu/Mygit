b442373733aa119be4c48fa2407d1eadf60ecee7
Alex87yu <ywg147258369@sina.com>
2019-09-30 15:10:28 +0800

    nice ####

--- Python/backup2.py
+++ Python/backup2.py
@@ -1,32 +1,21 @@
 #!/usr/bin/python
-
-# Filename: backup1.py
-
+# Filename: backup2.py
 import os
-
 import time
 
 # 1. The files and directories to be backed up are specified in a list.
 
 source = ['/data/data/com.termux/files']
-''' If you are using Windows, use source =
-[r'C:\Documents', r'D:\Work'] or something like that
+# If you are using Windows, use source =[r'C:\Documents', r'D:\Work'] or something like that
+# 2. The backup must be stored in a main backup directory/today make a new dir if today is not exist.
 
-# 2. The backup must be stored in a main backup
-
-directory
-'''
 target_dir = '/sdcard/backup/'
 
-''' Remember to change this
+#Remember to change this to what you will be using
+# 3. The files are backed up into a tar file.
 
-to what you will be using
+# 4. The name of the tar archive is commence +  the current time
 
-# 3. The files are backed up into a zip file.
-
-# 4. The name of the zip archive is the current date and
-
-time'''
 today = target_dir + time.strftime('%Y%m%d')
 now = time.strftime('%H%M%S')
 
@@ -35,27 +24,25 @@ now = time.strftime('%H%M%S')
 comment = input('Enter a comment --> ')
 
 if len(comment) == 0: # check if a comment was entered
-  target = today + os.sep + now + '_termux.tar.gz'
+  target = today + os.sep + now + '_termux.tar.gz'#os.sep means / . \ accodding to diff OS
 else:
   target = today + os.sep + now + '_' + \
 comment.replace(' ', '_') + '_termux.tar.gz'     # Notice the backslash!
-
-
-
+#using '_' instead of ' ' to filename 
 
 #target = target_dir + time.strftime('%Y%m%d%H%M%S') +'termux-backup.tar.gz'
-
-# 5. We use the zip command (in Unix/Linux) to put the files in a zip archive
 
 #Create the subdirectory if it isn't already there
 
 if not os.path.exists(today):
   os.mkdir(today) # make directory
   print ('Successfully created directory', today)
-# Run the backup
+
+# 5. We use the tar command (in Unix/Linux) to put the files in a tar  archive
 
 tar_command = "tar -zcvf '%s' %s" % (target, ' '.join(source))
 
+# Run the backup
 if os.system(tar_command) == 0:
   print ('Successful backup to', target)
 else:
